7c1e0707fdb2c5c581ad52ad47acb523ec3b6f2c
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-05-01 17:16:08 +0530

    added content in about and contact page

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.get("/",function(req,res){
 });
 
 
+app.get("/about",function(req,res){
+  res.render("about",{aboutContent:aboutContent})
+})
+
+app.get("/contact",function(req,res){
+  res.render("contact",{contactContent:contactContent})
+})
+
 
 
 
